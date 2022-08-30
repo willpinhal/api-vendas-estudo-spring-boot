04d4393aa42f1684e1cec2e8cf3e9761e380693f
william.souza <william.souza@puc-campinas.edu.br>
2022-08-30 19:17:54 -0300

    Commit inicial

---
+++ src/test/java/com/github/willpinhal/apivendas/apivendas/ApiVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.willpinhal.apivendas.apivendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
