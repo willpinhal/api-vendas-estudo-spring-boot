085eaf073a3b2f4416729801fb2ed1a4d902e7da
william.souza <william.souza@puc-campinas.edu.br>
2023-12-31 10:02:24 -0300

    Implementado os métodos para validar o token, obter o usuário do token e verificar se o token está válido.

--- src/main/java/com/github/willpinhal/apivendas/apivendas/JwtService.java
+++ src/main/java/com/github/willpinhal/apivendas/apivendas/JwtService.java
@@ -1,6 +1,8 @@
 package com.github.willpinhal.apivendas.apivendas;
 
 import com.github.willpinhal.apivendas.apivendas.domain.entities.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Configurable;
@@ -14,6 +16,7 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.HashMap;
 
 
 @Service
@@ -25,26 +28,59 @@ public class JwtService {
     @Value("${security.jwt.chave-assinatura}")
     private String chaveAssinatura;
 
-    public String gerarToken(Usuario usuario){
+    public String gerarToken(Usuario usuario) {
 
         long tempoExpiracao = Long.valueOf(expiracao);
         LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(tempoExpiracao);
         Instant instant = dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant();
         Date data = Date.from(instant);
 
+//        HashMap<String, Object> claims = new HashMap<>();
+//        claims.put("emailusuario", "usuario@gmail.com");
+//        claims.put("roles","admin");
+
         return Jwts
                 .builder()
                 .setSubject(usuario.getLogin())
                 .setExpiration(data)
+                //.setClaims(claims)
                 .signWith(SignatureAlgorithm.HS512, chaveAssinatura)
                 .compact();
     }
 
-    public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(ApiVendasApplication.class);
-        JwtService service = context.getBean(JwtService.class);
-        Usuario usuario = Usuario.builder().login("fulano").build();
-        String token = service.gerarToken(usuario);
-        System.out.println(token);
+    private Claims obterClaims(String token) throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(chaveAssinatura)
+                .parseClaimsJws(token)
+                .getBody();
     }
+
+    public boolean tokenValido(String token) {
+        try {
+            Claims claims = obterClaims(token);
+            Date dataExpiracao = claims.getExpiration();
+            LocalDateTime data = dataExpiracao.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            return LocalDateTime.now().isAfter(data);
+
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    public String obterUsuarioLogado(String token) {
+
+        return (String) obterClaims(token).getSubject();
+    }
+
+//    public static void main(String[] args) {
+//        ConfigurableApplicationContext context = SpringApplication.run(ApiVendasApplication.class);
+//        JwtService service = context.getBean(JwtService.class);
+//        Usuario usuario = Usuario.builder().login("fulano").build();
+//        String token = service.gerarToken(usuario);
+//        System.out.println("Token gerado -> " + token);
+//
+//        String login = service.obterUsuarioLogado(token);
+//        System.out.println("Usuário logado -> " + login);
+//    }
 }
