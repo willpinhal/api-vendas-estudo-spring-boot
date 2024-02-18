c43eb8151dc08377d3178877362a0570a1251891
william.souza <william.souza@puc-campinas.edu.br>
2024-02-18 00:41:41 -0300

    Adicionado o Swagger para documentar a API

--- src/main/java/com/github/willpinhal/apivendas/apivendas/config/InternacionalizacaoConfig.java
+++ src/main/java/com/github/willpinhal/apivendas/apivendas/config/InternacionalizacaoConfig.java
@@ -1,4 +1,4 @@
-package com.github.willpinhal.apivendas.apivendas.controllers.validations.config;
+package com.github.willpinhal.apivendas.apivendas.config;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
---
+++ src/main/java/com/github/willpinhal/apivendas/apivendas/config/SwaggerConfig.java
@@ -0,0 +1,67 @@
+package com.github.willpinhal.apivendas.apivendas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private Contact contact(){
+        return new Contact("William Souza", "https://github.com/willpinhal","teste@teste.com");
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Vendas API")
+                .description("API do projeto de vendas")
+                .version("1.0")
+                .contact(contact())
+                .build();
+    }
+
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.github.willpinhal.apivendas.apivendas.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+                .securityContexts(Arrays.asList( securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .apiInfo(apiInfo());
+    }
+
+    public ApiKey apiKey(){
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext(){
+        return SecurityContext.builder().securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any()).build();
+    }
+
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+
+        AuthorizationScope[] scopes = new AuthorizationScope[1];
+        scopes[0] = authorizationScope;
+        SecurityReference securityReference = new SecurityReference("JWT", scopes);
+        List<SecurityReference> auths = new ArrayList<>();
+        auths.add(securityReference);
+        return auths;
+    }
+}
